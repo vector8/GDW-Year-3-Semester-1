f4c9427b2fab5ddd740c79f4abaab94d6a3a492e
Chris <Chris@localhost>
2015-10-06 18:17:26 -0400

    Added some controls. Added "Coming Soon" animated text.

---
+++ ProjectLunarPunch/Assets/Prototype/Scripts/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenuGroup, vsAiGroup, sparGroup;
+
+    public float onScreenX, offScreenX;
+
+    public float moveTime;
+
+    private float currentMoveTime = 0f;
+
+    private void moveGroups(GameObject inFocus, GameObject outOfFocus1, GameObject outOfFocus2)
+    {
+        Vector3 pos = inFocus.transform.position;
+        pos.x = onScreenX;
+        inFocus.transform.position = pos;
+
+        pos = outOfFocus1.transform.position;
+        pos.x = offScreenX;
+        outOfFocus1.transform.position = pos;
+
+        pos = outOfFocus2.transform.position;
+        pos.x = offScreenX;
+        outOfFocus2.transform.position = pos;
+    }
+
+    public void goToMainMenuGroup()
+    {
+        moveGroups(mainMenuGroup, vsAiGroup, sparGroup);
+    }
+
+    public void goToVsAiGroup()
+    {
+        moveGroups(vsAiGroup, mainMenuGroup, sparGroup);
+    }
+
+    public void goToSparGroup()
+    {
+        moveGroups(sparGroup, mainMenuGroup, vsAiGroup);
+    }
+
+    public void exitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+    void Update()
+    {
+
+    }
+}
