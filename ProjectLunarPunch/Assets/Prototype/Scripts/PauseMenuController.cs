9b12c58c2738f71812c8974391b65046de0d4e80
Chris <Chris@localhost>
2015-10-07 15:51:56 -0400

    Added SampleBattle screen.

---
+++ ProjectLunarPunch/Assets/Prototype/Scripts/PauseMenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel.SetActive(!pausePanel.activeSelf);
+        }
+    }
+
+    public void exitToMainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
