8a37d2cdc195c75d45c1157a3d69ead78b143fd7
Chris <vector8@users.noreply.github.com>
2015-10-07 18:29:54 -0400

    Added shadows and started battle controls.

---
+++ ProjectLunarPunch/Assets/UnitControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitControl : MonoBehaviour
+{
+    private bool drawingLine = false;
+    private Vector2 lineStart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.name.StartsWith("Unit"))
+                {
+                    drawingLine = true;
+                    lineStart = Input.mousePosition;
+                }
+            }
+        }
+        else if (!Input.GetMouseButton(0) && drawingLine)
+        {
+            drawingLine = false;
+        }
+        
+
+        if(drawingLine)
+        {
+            
+        }
+    }
+}
