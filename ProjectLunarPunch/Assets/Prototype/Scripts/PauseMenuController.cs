90194ce8315721f1a07ede78e48c87ba2af48052
Chris <vector8@users.noreply.github.com>
2015-10-07 23:51:31 -0400

    Added a timer and line drawing when giving orders to units.

--- ProjectLunarPunch/Assets/Prototype/Scripts/PauseMenuController.cs
+++ ProjectLunarPunch/Assets/Prototype/Scripts/PauseMenuController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PauseMenuController : MonoBehaviour
 {
     public GameObject pausePanel;
+    public Button lockInButton;
+    public TimerController timer;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             pausePanel.SetActive(!pausePanel.activeSelf);
+            lockInButton.interactable = !lockInButton.interactable;
+            timer.paused = !timer.paused;
         }
     }
 
---
+++ ProjectLunarPunch/Assets/TimerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public Text timerText;
+    public bool paused = false;
+
+    private float timer = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!paused && timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            if(timer < 0f)
+            {
+                timer = 0f;
+            }
+            timerText.text = ((int)timer).ToString();
+        }
+    }
+
+    public void resetTimer()
+    {
+        timer = 30f;
+    }
+}
