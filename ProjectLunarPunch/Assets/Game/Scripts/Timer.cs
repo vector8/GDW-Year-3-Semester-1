3c2e656b5d1934c00324445ed206fed8802fc208
Chris <Chris@localhost>
2015-10-18 23:04:01 -0400

    Moved stuff from old Prototype/SampleBattle scene into new Game/Battle scene. Created prefabs for all the units. Added basic attack functionality using stats from the word doc created by Mike. Modified targeting mechanism to give the lines random colours. Still need to add some kind of basic attack animation (such as moving the model forward and back quickly) as well as visual display for HP.

---
+++ ProjectLunarPunch/Assets/Game/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer
+{
+    private float time = 0f;
+
+    public void setTime(float t)
+    {
+        time = t;
+    }
+
+    public void update(float elapsed)
+    {
+        time -= elapsed;
+        if(time < 0f)
+        {
+            time = 0f;
+        }
+    }
+
+    public bool isDone()
+    {
+        return Mathf.Approximately(time, 0f);
+    }
+}
---
+++ ProjectLunarPunch/Assets/Game/Scripts/Unit.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ClassType
+{
+    Archer,
+    Berserker,
+    Horseman,
+    Spearman,
+    Swordsman,
+    Warhound
+}
+
+public class Unit : MonoBehaviour
+{
+    // TODO: put real stats here
+    public float hp, maxHp, att, def, speed, critChance;
+    public ClassType type;
+    public bool defending = false;
+    public bool ally = false;
+
+    private const float CRIT_BONUS = 1.5f;
+    private const float ADVANTAGE_BONUS = 1.2f;
+    private const float DEFENDING_MODIFIER = 0.5f;
+
+    public void attack(Unit other)
+    {
+        if(!other.gameObject.activeSelf)
+        {
+            return;
+        }
+
+        bool crit = (Random.Range(0f, 100f) < critChance);
+
+        float damage = att * ((100f - other.def) / 100f);
+
+        if(hasAdvantage(type, other.type))
+        {
+            damage *= ADVANTAGE_BONUS;
+        }
+
+        if (crit)
+        {
+            // TODO: add special logic for berserkers and swordsmen.
+            damage *= CRIT_BONUS;
+            print("Critical hit!");
+        }
+
+        if (other.defending)
+        {
+            damage *= DEFENDING_MODIFIER;
+        }
+
+        // TODO: Add animation logic?
+        print(gameObject.name + " attacking " + other.gameObject.name + " for " + damage + " damage.");
+
+        other.takeDamage(damage);
+    }
+
+    public void takeDamage(float damage)
+    {
+        hp -= damage;
+        if(hp <= 0f)
+        {
+            hp = 0f;
+
+            // TODO: death logic here...
+            gameObject.SetActive(false);
+            print(gameObject.name + " has died!");
+        }
+        else
+        {
+            print(gameObject.name + " has " + hp + " HP remaining.");
+        }
+    }
+
+    public static bool hasAdvantage(ClassType first, ClassType second)
+    {
+        switch(first)
+        {
+            case ClassType.Archer:
+                return second == ClassType.Spearman;
+            case ClassType.Berserker:
+                return second == ClassType.Warhound;
+            case ClassType.Horseman:
+                return second == ClassType.Swordsman;
+            case ClassType.Spearman:
+                return second == ClassType.Horseman;
+            case ClassType.Swordsman:
+                return second == ClassType.Berserker;
+            case ClassType.Warhound:
+                return second == ClassType.Archer;
+            default:
+                return false;
+        }
+    }
+}
