a1676052008cc35642833bc1b3fb1d44aa4082ae
ishraf <ishraf@localhost>
2015-11-09 13:18:18 -0500

    got something instantiating from the buttons

---
+++ ProjectLunarPunch/Assets/Game/Scripts/ArmyBuildSpawnSoldier.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmyBuildSpawnSoldier : MonoBehaviour
+{
+
+    public GameObject soldierTest;
+
+    public GameObject[] spawnLoc;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnCharacter()
+    {
+        Instantiate(soldierTest, spawnLoc[0].transform.position, spawnLoc[0].transform.rotation);
+    }
+
+
+}
