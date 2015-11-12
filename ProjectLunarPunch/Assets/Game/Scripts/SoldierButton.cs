69c3f1c69dae771ee4dac62bc959895bcb4dc68e
ishraf <ishraf@localhost>
2015-11-12 14:26:56 -0500

    chris did something with the logic for the button gota work on it further

---
+++ ProjectLunarPunch/Assets/Game/Scripts/SoldierButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoldierButton : MonoBehaviour
+{
+    private Button pressedButton = null;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    public void buttonPress(Button caller)
+    {
+        if(pressedButton != null)
+        {
+            pressedButton.interactable = true;
+        }
+
+        pressedButton = caller;
+        pressedButton.interactable = false;
+    }
+
+    public bool getButtonState()
+    {
+        return pressedButton.IsInteractable();
+    }
+}
