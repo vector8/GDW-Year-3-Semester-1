308717325584f3fb2ae7a4891d8fdc24b423911f
ishraf <ishraf@localhost>
2015-11-15 18:32:58 -0500

    put in the dog and spearman models with the textures we created. decided to make some changes to the way the army builder does its thing so far. trying to make everything work of one script. now how to figure out putting everything in arrays

--- ProjectLunarPunch/Assets/Game/Scripts/SoldierButton.cs
+++ ProjectLunarPunch/Assets/Game/Scripts/SoldierButton.cs
@@ -5,25 +5,59 @@ using System.Collections;
 public class SoldierButton : MonoBehaviour
 {
     private Button pressedButton = null;
+    public bool buttonCheck;
 
+    public GameObject[] soldierTypes;
     // Use this for initialization
     void Start()
     {
+       
+    }
+
+    void Update()
+    {
+       if (pressedButton != null)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "SpawnSphere" && Input.GetMouseButtonDown(0))
+                {
+
+
+                    Debug.Log(hit.collider.name);
+
+                    if(pressedButton.name.Contains("dog"))
+                    {
+                        Instantiate(soldierTypes[0], hit.transform.position, hit.transform.rotation);
+                    }
+                    if(pressedButton.name.Contains("spearman"))
+                    {
+                        Instantiate(soldierTypes[1], hit.transform.position, hit.transform.rotation);
+                        
+                    }
+                }
+            }
+        }
+
     }
 
     public void buttonPress(Button caller)
     {
-        if(pressedButton != null)
+        Debug.Log(caller.name + " is active");
+        if (pressedButton != null)
         {
             pressedButton.interactable = true;
+            buttonCheck = pressedButton.IsInteractable();
         }
+
+      
 
         pressedButton = caller;
         pressedButton.interactable = false;
-    }
-
-    public bool getButtonState()
-    {
-        return pressedButton.IsInteractable();
+        buttonCheck = pressedButton.IsInteractable();
     }
 }
