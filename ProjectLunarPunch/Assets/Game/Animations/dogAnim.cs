174a225b1db57ab4efac76a0907d4bd478a3d5cf
ishraf <ishraf@localhost>
2016-02-23 23:20:17 -0500

    starting to understand how animation logic can be implemented, so far only some animation clips work with each other. Only solution is to place all animations in same maya file before importing them into work space. For now dog only animations in game.

---
+++ ProjectLunarPunch/Assets/Game/Animations/dogAnim.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dogAnim : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("run");
+            anim.SetBool("isRun", true);
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            anim.SetBool("isRun", false);
+        }
+	
+	}
+}
