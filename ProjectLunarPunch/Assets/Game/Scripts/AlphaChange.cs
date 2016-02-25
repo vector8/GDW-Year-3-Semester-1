9ed6ec7a2658adf4ba7a473a7c63112c0b054404
Curtis <Curtis@localhost>
2016-02-25 18:35:11 -0500

    Started to work on fading stuff.

---
+++ ProjectLunarPunch/Assets/Game/Scripts/AlphaChange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaChange : MonoBehaviour
+{
+    public Material material;
+    public float fadeInterval;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (material.color.a != 255)
+        {
+            material.color = new Color(255, 255, 255, fadeInterval * Time.deltaTime);
+        }
+    }
+}
