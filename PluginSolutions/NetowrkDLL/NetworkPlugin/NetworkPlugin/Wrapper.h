2570f0bf5a5ac56812310338d48114c886aebd79
Curtis <Curtis@localhost>
2016-02-29 17:02:25 -0500

    Started dll wrapper. restructured network dll

---
+++ PluginSolutions/NetowrkDLL/NetworkPlugin/NetworkPlugin/Wrapper.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "LibSettings.h"
+#include "Network.h"
+
+#ifdef __cplusplus
+extern"C"
+{
+#endif
+
+	LIB_API char setServer();
+	LIB_API void initialize();
+	LIB_API void update();
+
+
+#ifdef __cplusplus
+}
+#endif
