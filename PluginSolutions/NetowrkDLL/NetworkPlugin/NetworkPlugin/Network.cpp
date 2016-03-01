d2f930086ca15d9f8913f3978c7587b6e41c25c3
Curtis <Curtis@localhost>
2016-03-01 16:51:56 -0500

    Pushing to work on other DLL on other PC. No notable changes.

--- PluginSolutions/NetowrkDLL/NetworkPlugin/NetworkPlugin/Network.cpp
+++ PluginSolutions/NetowrkDLL/NetworkPlugin/NetworkPlugin/Network.cpp
@@ -1,5 +1,10 @@
 #include "Network.h"
 
+Network::Network()
+{
+
+}
+
 char Network::setServer()
 {
 	//server = SERVER;
@@ -19,7 +24,7 @@ void Network::initialize()
 	memset((char *)&sockAddr, 0, sizeof(sockAddr));
 	sockAddr.sin_family = AF_INET;
 	sockAddr.sin_port = htons(8888);
-	sockAddr.sin_addr.S_un.S_addr = inet_addr(SERVER);
+	sockAddr.sin_addr.S_un.S_addr = inet_addr(server);
 
 	//set blocking / non blocking (1 = non blocking)
 	u_long iMode = 1;
@@ -28,7 +33,11 @@ void Network::initialize()
 
 void Network::update()
 {
+	//send packets
 	sprintf_s(msg, sizeof(msg), "%f", msg);
-
 	sendto(s, buf, strlen(msg), 0, (struct sockaddr *) &sockAddr, slen);
+
+	//receive packets
+	recv_len = recv(s, buf, 512, 0);
+	
 }
