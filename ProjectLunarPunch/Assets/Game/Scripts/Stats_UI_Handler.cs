5619a68d91d68cee6d1d8efbe23648f1236d8b4c
Brewstertyler <Brewstertyler@localhost>
2016-03-04 17:06:25 -0500

    Dear Diary,         Today we worked on the SoldierButton class, and the Stats_UI_Handler         class. It was so much fun :) I really enjoyed the time when Tyler said         something funny, and we all laughed!! It was awesome.         but seriously i had asked david for help and he started reformating the         way we spwan and store soldiers. THis is writen ishraf btw. sooooo im no         longer doing multiple char parsing to get int and float values so it         should be faster

--- ProjectLunarPunch/Assets/Game/Scripts/Stats_UI_Handler.cs
+++ ProjectLunarPunch/Assets/Game/Scripts/Stats_UI_Handler.cs
@@ -9,33 +9,39 @@ public class Stats_UI_Handler : MonoBehaviour {
     public GameObject arrayhandle;
 
     public GameObject[] arry = new GameObject[5];
-
     public Canvas localHandle;
 
     public GameObject spot;
 
-    int spotValue;
+    public int spotValue;
 
+    // This will turn the canvas on and off
+    // The SoldierButton class calls with function with
+    // GameObject.SendMessage("activeUI", true/false);
+    public void activeUI(bool activate)
+    {
+        transform.GetChild(0).gameObject.SetActive(activate);
+    }
 
 	// Use this for initialization
 	void Start () {
-
+        // Get a handle to the SoliderButton List (it has the game objects
+        // of all the units on the map)
        arry = arrayhandle.GetComponent<SoldierButton>().g;
-       spotValue = int.Parse(spot.name);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-       // activeBut = ((buttonLocations)arrayhandle.GetComponent<SoldierButton>().ID);
-
-
-        if(arry[spotValue-1] != null)
+    void Update()
+    {
+        // If the array isn't null, and the canvas is active
+        if (arry[spotValue] != null && transform.GetChild(0).gameObject.activeInHierarchy)
         {
-            localHandle.GetComponentsInChildren<Text>()[0].text = arry[spotValue - 1].GetComponent<Unit>().hp.ToString();
-            localHandle.GetComponentsInChildren<Text>()[1].text = arry[spotValue - 1].GetComponent<Unit>().speed.ToString();
-            localHandle.GetComponentsInChildren<Text>()[2].text = arry[spotValue - 1].GetComponent<Unit>().att.ToString();
-            localHandle.GetComponentsInChildren<Text>()[3].text = arry[spotValue - 1].GetComponent<Unit>().def.ToString();
+            // Set all of the text components in the canvas to the Stats of the current Unit
+            // in that position
+            localHandle.GetComponentsInChildren<Text>()[0].text = arry[spotValue].GetComponent<Unit>().hp.ToString();
+            localHandle.GetComponentsInChildren<Text>()[1].text = arry[spotValue].GetComponent<Unit>().speed.ToString();
+            localHandle.GetComponentsInChildren<Text>()[2].text = arry[spotValue].GetComponent<Unit>().att.ToString();
+            localHandle.GetComponentsInChildren<Text>()[3].text = arry[spotValue].GetComponent<Unit>().def.ToString();
         }
-
-	}
+    }
 }
