707a7845d294d8fe939fd228c0df5575f3a317dd
Michael Van Vaals <Michael Van Vaals@localhost>
2016-03-09 03:23:39 -0500

    [ADD] New archer with animations, with some hacky code that will need to be changed after the review.

--- ProjectLunarPunch/Assets/Game/Scripts/Archer.cs
+++ ProjectLunarPunch/Assets/Game/Scripts/Archer.cs
@@ -13,15 +13,20 @@ public class Archer : Unit
         {
             arrow = Instantiate<GameObject>(arrowPrefab);
             attackState = AttackState.Returning;
-            gameObject.transform.LookAt(target.transform, new Vector3(0f, 1f, 0f));
+
+            //TODO: remove this
+            Vector3 Target = target.transform.position;
+            Target.y = 90;
+            
+            gameObject.transform.LookAt(Target, new Vector3(0f, 1f, 0f));
         }
         else if (attackState == AttackState.Returning)
         {
             attackTimer.update(Time.deltaTime);
 
             Vector3 yOffset = new Vector3(0f, 100f, 0f);
-
-            arrow.transform.position = Vector3.Lerp(target.transform.position + yOffset, originalPosition + yOffset, attackTimer.getTime() / ATTACK_ANIMATION_TIME);
+            Vector3 yOffset2 = new Vector3(0f, 10f, 0f);
+            arrow.transform.position = Vector3.Lerp(target.transform.position + yOffset, originalPosition + yOffset2, attackTimer.getTime() / ATTACK_ANIMATION_TIME);
             arrow.transform.LookAt(target.transform.position + yOffset);
 
             if (attackTimer.isDone())
