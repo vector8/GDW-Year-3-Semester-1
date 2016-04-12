03e08b7a3b949be995416e808b22879378e72ff2
Michael Van Vaals <Michael Van Vaals@localhost>
2016-04-12 07:22:18 -0400

    More Networking Changes

---
+++ ProjectLunarPunch/Assets/IPTextBox.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IPTextBox : MonoBehaviour {
+
+	public void TextChange()
+    {
+        NetworkWrapper.serverIP = this.GetComponentInChildren<Text>().text;
+    }
+}
